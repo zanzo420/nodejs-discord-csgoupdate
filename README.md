5ff0454c4aa3b971d11f1005cfc69312a980d31a
Triniayo <leif.muscheites96@gmx.de>
2018-10-30 15:06:17 +0100

    v1.3.0 release

---
+++ README.md
@@ -0,0 +1,55 @@
+# Node.js Discord CSGO Update Bot
+
+This is a Node.js Discord Bot which sends CS:GO Updates to a specific channel.
+
+If you find any bugs, please do not hesitate to report them to me.
+
+[![GitHub issues](https://img.shields.io/github/issues/Triniayo/nodejs-discord-csgoupdate.svg)](https://github.com/Triniayo/nodejs-discord-csgoupdate/issues)
+[![GitHub stars](https://img.shields.io/github/stars/Triniayo/nodejs-discord-csgoupdate.svg)](https://github.com/Triniayo/nodejs-discord-csgoupdate/stargazers)
+[![GitHub license](https://img.shields.io/github/license/Triniayo/nodejs-discord-csgoupdate.svg)](https://github.com/Triniayo/nodejs-discord-csgoupdate)
+[![Twitter](https://img.shields.io/twitter/url/https/github.com/Triniayo/nodejs-discord-csgoupdate.svg?style=social)](https://twitter.com/intent/tweet?text=Wow:&url=https%3A%2F%2Fgithub.com%2FTriniayo%2Fnodejs-discord-csgoupdate)
+
+# Changelog
+```
+v1.1.0
+- Added message limiting
+- Ghetto fix to prevent message spam
+
+v1.2.0
+- Completely revised
+- Improved code
+- Removed ghetto fixes
+- Removed MySQL requirement
+- Added textfile to log date of last update
+- Set message limiting to a maximum of 10 lines (looks way better now - see below)
+
+v1.3.0
+- Changed to another RSS module
+- Recoded whole RSS feed stuff
+- Spams have been fixed by changing RSS package
+```
+
+# Preview
+
+![Preview Image](https://raw.githubusercontent.com/Triniayo/nodejs-discord-csgoupdate/master/preview.png)
+
+# Requirements
+
+* Node.js
+
+# Setup
+
+```
+git clone https://github.com/Triniayo/nodejs-discord-csgoupdate.git
+npm install
+```
+
+### ✏️ Edit `config.json`
+
+### ▶️ Run `npm start` to start the bot
+
+### 🎓 You are done.
+
+### 🤖 Authors
+
+* Triniayo
---
+++ app.js
@@ -0,0 +1,105 @@
+// Requirements
+const { Client } = require('discord.js');
+const fs = require('fs');
+let Parser = require('rss-parser');
+const htmlToText = require('html-to-text');
+const log = require('npmlog');
+
+// Load Config
+const cfg = require('./config.json');
+
+// Create feeder instance
+let rssParser = new Parser({
+    // Renaming 'content:encoded' to 'contentHtml' because it won't be useable otherwise
+    customFields: {
+        item: [
+            ['content:encoded', 'contentHtml']
+        ]
+    },
+    // Setting User Agent
+    headers: {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/69.0.3497.100 Safari/537.36'
+    }
+});
+
+// Setting variables for the RSS output
+let updateTitle;
+let updateURL;
+let updateContent;
+let updateDate;
+
+// Setting last update variable
+let lastUpdate;
+
+function getLastUpdate() {
+    // Getting date of last update from check-update.txt file
+    let dateFromFile = fs.readFileSync('check-update.txt');
+    lastUpdate = dateFromFile.toString();
+}
+
+// Getting last date from check-update.txt file every second
+setInterval(getLastUpdate, 5000);
+
+// Create Discord Bot Instance
+const bot = new Client();
+
+// Logging into Bot Account
+bot.on('ready', () => {
+    log.info('discord', `Logged in as ${bot.user.tag} (User ID: ${bot.user.id}) on ${bot.guilds.size} server(s)`);
+    bot.user.setActivity('Fetching Updates');
+
+    // Checking every 15 seconds, if there's a new update, and if it's been posted already
+    setInterval(getUpdate, 15000);
+});
+
+function getUpdate() {
+    // Get current date and edit format
+    let getDate = new Date();
+    let date = (getDate.getFullYear() +'-'+ (getDate.getMonth() +1) +'-'+ getDate.getDate());
+
+    // Fetching updates from CS:GO Blog
+    (async () => {
+        // Setting RSS Feed URL
+        let feed = await rssParser.parseURL('http://blog.counter-strike.net/index.php/category/updates/feed/');
+
+        // Setting items into variables
+        feed.items.forEach(item => {
+            updateTitle = item.title;
+            updateURL = item.link;
+            updateContent = item.contentHtml;
+            updateDate = item.isoDate;
+        });
+
+        // Converting from HTML to readable text
+        let format = htmlToText.fromString(updateContent, {
+            wordwrap: 130
+        });
+
+        // Limiting the update to 10 lines
+        let updateNotes = format.split('\n', 10);
+
+        if (updateDate.includes(`${date}`)) {
+            if (lastUpdate !== date) {
+                bot.channels.get(cfg.channelid).send("@everyone A new CS:GO Update has been released!", {
+                    embed: {
+                      "title": `${updateTitle}`,
+                      "description": `${updateNotes.join('\n')}...\n\n[Continue reading on the CS:GO Blog](${updateURL})`,
+                      "url": `${updateURL}`,
+                      "color": 5478908,
+                      "thumbnail": {
+                        "url": "https://raw.githubusercontent.com/Triniayo/nodejs-discord-csgoupdate/master/csgo-icon.png"
+                      }
+                    }
+                });
+
+                // Storing date of the latest update in the check-update.txt
+                fs.writeFileSync('check-update.txt', `${date}`)
+            }
+        } else {
+            // Do nothing if update has been posted already.
+        }
+    })();
+}
+
+// Logging into the Bot Account
+bot.login(cfg.token);
